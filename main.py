3afc4fabf2780c1604b2516e680c26d974b3394e
abdalrzag eisa <bmjfdrh@gmail.com>
2023-08-01 20:12:57 +0300

    fetch chapters

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import requests
 import argparse
+import time
 from dataclasses import dataclass, field
+
+
+def parse_filename(f) -> tuple[str, str]:
+    num = ""
+    parts = f.split("-")
+    page_number = parts[0]
+    file_ext = parts[1].split(".")[1]
+
+    for char in page_number:
+        if not char.isdigit():
+            continue
+
+        num += char
+
+    return (num, f"{num}.{file_ext}")
 
 
 @dataclass
@@ -73,16 +89,98 @@ class MangaInfo:
         return self
 
 
+@dataclass
+class Page:
+    num: int = field(default_factory=int)
+    url: str = field(default_factory=str)
+    file_name: str = field(default_factory=str)
+
+
+def fetch_pages(chapter_id: str) -> list[Page]:
+    url = f"https://api.mangadex.org/at-home/server/{chapter_id}?forcePort443=false"
+
+    res = requests.get(url)
+
+    res_json = res.json()
+
+    pages = []
+
+    chapter = res_json["chapter"]
+    hash = chapter["hash"]
+    data = chapter["data"]
+
+    for filename in data:
+        page_url = f"https://uploads.mangadex.org/data/{hash}/{filename}"
+        parsed_filename = parse_filename(filename)
+        new_page = Page(int(parsed_filename[0]), page_url, parsed_filename[1])
+
+        pages.append(new_page)
+
+    return pages
+
+
+@dataclass
+class Chapter:
+    id: str
+    volume: str
+    number: str
+    title: str
+
+
+def fetch_chapters(manga_id) -> list[Chapter]:
+    print("Fetching chapters")
+    offset: int = 0
+    limit: int = 96
+    chapters = []
+
+    while True:
+        url = f"https://api.mangadex.org/manga/{manga_id}/feed?translatedLanguage[]=en&limit={limit}&order[volume]=asc&order[chapter]=asc&offset={offset}"
+        res = requests.get(url)
+
+        json_res = res.json()
+        data: list = json_res["data"]
+
+        # If data is empty that means we reached the end of manga
+        if len(data) == 0:
+            break
+
+        print("Fetching chapters")
+
+        for chapter in data:
+            id = chapter["id"]
+            ch_type = chapter["type"]
+
+            if ch_type != "chapter":
+                continue
+
+            attr = chapter["attributes"]
+
+            volume = attr["volume"]
+            num = attr["chapter"]
+            title = attr["title"]
+
+            if attr["externalUrl"]:
+                continue
+
+            new_chapter = Chapter(id, volume, num, title)
+
+            chapters.append(new_chapter)
+
+        offset = offset + limit
+        time.sleep(0.600)
+
+    return chapters
+
+
 class Manga:
     def __init__(self, id: str):
         self.id = id
-        self.info = MangaInfo(id).fetch()
+        self.info: MangaInfo = MangaInfo(id).fetch()
+        self.chapters: list[Chapter] = fetch_chapters(id)
 
 
 def main(manga_id: str):
     manga = Manga(manga_id)
-
-    print(manga.info)
 
 
 if __name__ == "__main__":
